48dd75eaf58275d76d7a2846b57537d953e25d2a
ariasbruno <163929731+ariasbruno@users.noreply.github.com>
2024-10-24 00:53:01 -0300

    fix

--- scripts/components/use_interactables/useModal.js
+++ scripts/components/use_interactables/useModal.js
@@ -1,3 +1,8 @@
+const $ =  selector => document.querySelector(selector);
+const $$ =  selector => document.querySelectorAll(selector);
+
+const allButtonsDefault = [...$("#toolbar").children];
+
 export function modalAction(action) {
 const $noteModal = document.querySelector("#note_modal");
 const $inputTitle = document.querySelector("#title");
@@ -28,10 +33,12 @@ $inputContent.addEventListener('input', () => adjustInputHeight($inputContent));
     $body.style.overflow = "hidden";
     $noteModal.style.display = "flex";
     adjustModalSizes();
+    window.addEventListener("resize", handleOverflowIcons);
     
   } else if (action === 'close') {
     $noteModal.style.display = "none";
     $body.style.overflow = "";
+    window.removeEventListener("resize", handleOverflowIcons);
   }
 
   $noteModal.addEventListener("click", e => {
@@ -72,4 +79,60 @@ $inputContent.addEventListener('input', () => adjustInputHeight($inputContent));
     selection.removeAllRanges();
     selection.addRange(range);
   });
+
+  handleOverflowIcons();
+  function handleOverflowIcons() {
+  const $toolbar = document.getElementById('toolbar');
+  const $toolbarIcons = $toolbar.querySelectorAll('.btn_toolbar');
+  const $dropdown = document.getElementById('dropdown');
+  const $dropdownOpen = document.getElementById('dropdown-open');
+  const $dropdownContainer = document.getElementById('dropdown-container');
+  
+    $dropdownContainer.innerHTML = '';
+    allButtonsDefault.forEach(e => {
+      $toolbar.appendChild(e)
+    })
+
+    let num = 0
+    
+    allButtonsDefault.forEach(button => {
+      if (button.id === "dropdown") return;
+      num += 1
+    });
+    
+    console.log(num)
+    
+    
+    const toolbarWidth = $toolbar.offsetWidth;
+    const toolbarPadding = 10;
+    let usedWidth = ((num - 1) * (32 + toolbarPadding)) + (94 * 2 + toolbarPadding) + 32;
+    console.log('Used width:', usedWidth);
+    console.log('Toolbar width:', toolbarWidth);
+
+    if (toolbarWidth < ((num - 2) * (32 + toolbarPadding)) + (94 * 2 + toolbarPadding) + 32) {
+      for (let i = num - 1; toolbarWidth < usedWidth && i >= 0; i--) {
+        const buttonWidth = allButtonsDefault[i].offsetWidth || 32;
+        usedWidth = usedWidth - (buttonWidth + toolbarPadding);
+        console.log('Removed button:', allButtonsDefault[i]);
+      
+        if (!$dropdownContainer.contains(allButtonsDefault[i])) {
+          $dropdownContainer.appendChild(allButtonsDefault[i]);          
+
+        }
+      }
+    }
+
+    if ($dropdownContainer.children.length > 0) {
+      $dropdown.style.display = 'flex';
+      $dropdownOpen.addEventListener("click", openDropdown)
+    } else {
+      $dropdown.style.display = 'none';
+      $dropdownOpen.removeEventListener("click", openDropdown)
+    }
+  }
+  
+  function openDropdown() {
+    document.querySelector('#dropdown-container').classList.toggle('show');
+    document.querySelector('#dropdown-open-svg').classList.toggle('rotate');
+  }
 };
